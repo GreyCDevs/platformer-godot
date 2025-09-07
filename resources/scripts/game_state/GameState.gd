157611ec3bea4859818b68544230c1972f619b4b
GreyCDevs <raulpavonfp@gmail.com>
2025-09-07 13:14:46 +0200

    fixed: error in dead

--- resources/scripts/game_state/GameState.gd
+++ resources/scripts/game_state/GameState.gd
@@ -6,9 +6,10 @@ var is_player_dead: bool = false
 
 func remove_player_lives(lives: int) -> void:
 	player_lives -= lives
-	if player_lives >= 0:
+	if player_lives <= 0:
 		is_player_dead = true
 		player_lives = 0
  
 func reset_player_lives() -> void:
 	player_lives = MAX_PLAYER_LIVES
+	is_player_dead = false
